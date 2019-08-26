1787b3272c8e48c65fffc0668ea9319d88ab7238
Gerardo Paredes <gerardoparedes@gerardos-mbp.home>
2019-08-26 16:54:07 -0700

    imageList, categoryTag, unit tests added

--- src/tests/Components/UI/CategoryTag/CategoryTag.test.js
+++ src/tests/Components/UI/CategoryTag/CategoryTag.test.js
@@ -3,7 +3,20 @@ import { shallow } from 'enzyme';
 import CategoryTag from '../../../../components/UI/CategoryTag/CategoryTag';
 
 describe('renders <CategoryTag /> component with correct props', () => {
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<CategoryTag />);
+    });
+
+    test('should render tag without props', () => {
+        expect(wrapper.find('.tag__title').text()).toEqual('Free images of ');
+    });
+
     test('should render tag with props', () => {
-        
+        wrapper.setProps({ tag: 'Summer' });
+        expect(wrapper.find('.tag__wrap')).toHaveLength(1);
+        expect(wrapper.find('.tag__title')).toHaveLength(1);
+        expect(wrapper.find('.tag__title').text()).toEqual('Free images of Summer');
     });
 });
--- src/tests/Components/UI/ImageList/ImageList.test.js
+++ src/tests/Components/UI/ImageList/ImageList.test.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import ImageList from '../../../../components/UI/ImageList/ImageList';
+import ImageListItem from '../../../../components/UI/ImageList/ImageListItem';
 
 describe('renders <ImageList /> component with correct props', () => {
+    let wrapper;
+    const images = [{
+        url: 'http://someTestUrl.com',
+        id: 1
+    },{
+        url: 'http://someTestUrl.com',
+        id: 2
+    }];
+
+    beforeEach(() => {
+        wrapper = shallow(<ImageList />);
+    });
+
     test('should render imageList with props', () => {
-        
+        wrapper.setProps({ images: images });
+        expect(wrapper.find(ImageListItem)).toHaveLength(2);
+    });
+
+    test('should not render imageList with props', () => {
+        wrapper.setProps({ images: [] });
+        expect(wrapper.find(ImageListItem)).toHaveLength(0);
     });
 });
